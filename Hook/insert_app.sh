246fb3a69a23ce3400e02710fde48742559f7152
王靖凯 <wjk930726@live.com>
2018-02-12 02:40:19 +0800

    move your body every every body

--- Hook/insert_app.sh
+++ Hook/insert_app.sh
@@ -7,11 +7,11 @@ tweak=`ls | grep *.xm | head -1`
 temp='x11901'
 name=${path%.app}
 
-$THEOS/bin/logos.pl "./${tweak}" > "./${temp}.m"
-clang -shared -undefined dynamic_lookup -o "./${path}/Contents/MacOS/lib.dylib" "./${temp}.m"
+$THEOS/bin/logos.pl "./${tweak}" > "./${temp}.mm"
+clang -shared -undefined dynamic_lookup -o "./${path}/Contents/MacOS/lib.dylib" "./${temp}.mm"
 optool install -c load -p @executable_path/lib.dylib -t "./${path}/Contents/MacOS/${name}"
 
-rm -f ${temp}.m
+rm -f "${temp}.mm"
 
 # 使用unsign效果可能更好，codesign --remove-signature 在删除代码签名之后没有修复MachO Header的偏移，导致生成的MachO文件畸形
 # codesign --remove-signature ${name}
